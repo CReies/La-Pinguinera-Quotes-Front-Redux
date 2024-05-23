0a4d815b764622e829b4e66de548ecf0248c229a
= <ccreyesmolina@gmail.com>
2024-05-23 01:04:04 -0500

    Add GetQuoteAggregateResponseMapper

---
+++ src/app/core/mappers/api/response/get-quote-aggregate-response.mapper.ts
@@ -0,0 +1,11 @@
+import { Injectable } from "@angular/core";
+import { IGetQuoteAggregateResponse } from "../../../models/api/response/get-quote-aggregate-response.model";
+
+@Injectable({providedIn:'root'})
+export class GetQuoteAggregateResponseMapper{
+	map(payload: any): IGetQuoteAggregateResponse {
+		return{
+			aggregateId: payload.aggregateId
+		}
+	}
+}
