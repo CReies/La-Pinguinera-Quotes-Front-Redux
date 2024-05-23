f587525b0f3d018d2a01beb74a636f5a1ebe3214
= <ccreyesmolina@gmail.com>
2024-05-23 06:00:19 -0500

    Add main components for the home page

---
+++ src/app/ui/blocks/calculate-group/calculate-group.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-calculate-group',
+  standalone: true,
+  imports: [],
+  templateUrl: './calculate-group.component.html',
+  styleUrl: './calculate-group.component.css'
+})
+export class CalculateGroupComponent {
+
+}
