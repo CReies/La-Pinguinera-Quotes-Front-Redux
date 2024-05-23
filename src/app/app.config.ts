29fd047eb58916caf347f12b1cde671cb9cf5c3c
= <ccreyesmolina@gmail.com>
2024-05-23 09:12:15 -0500

    Refactor app.config.ts to provide store and effects for appStore and appEffects

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -5,7 +5,13 @@ import { routes } from './app.routes';
 import { provideHttpClient } from '@angular/common/http';
 import { provideStore } from '@ngrx/store';
 import { provideEffects } from '@ngrx/effects';
+import { appEffects, appStore } from './core/store/store';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideHttpClient(), provideStore(), provideEffects()]
+  providers: [
+    provideRouter(routes),
+    provideHttpClient(),
+    provideStore(appStore),
+    provideEffects(appEffects),
+  ],
 };
