9ed01ba19b34ae5e71d4a08f2c1dcde1d9ca30d8
= <ccreyesmolina@gmail.com>
2024-05-23 06:06:50 -0500

    Refactor routing module to use container components for book list and group cart

--- src/app/public/public-routing.module.ts
+++ src/app/public/public-routing.module.ts
@@ -1,16 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LayoutMainComponent } from '../ui/layouts/layout-main/layout-main.component';
-import { BookListComponent } from '../ui/blocks/book-list/book-list.component';
-import { CalculateGroupComponent } from '../ui/blocks/calculate-group/calculate-group.component';
+import { BookListContainerComponent } from '../containers/book-list-container/book-list-container.component';
+import { GroupCartContainerComponent } from '../containers/group-cart-container/group-cart-container.component';
 
 const routes: Routes = [
   {
     path: '',
     component: LayoutMainComponent,
     children: [
-      { path: '', component: BookListComponent, outlet: 'main' },
-      { path: '', component: CalculateGroupComponent, outlet: 'aside' },
+      { path: '', component: BookListContainerComponent, outlet: 'main' },
+      { path: '', component: GroupCartContainerComponent, outlet: 'aside' },
     ],
   },
   {
