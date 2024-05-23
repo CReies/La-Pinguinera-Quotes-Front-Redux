fbe40c8a487be718273b64af0804158fb1c24087
= <ccreyesmolina@gmail.com>
2024-05-23 06:00:34 -0500

    Add styles to the main layout

--- src/app/ui/layouts/layout-main/layout-main.component.ts
+++ src/app/ui/layouts/layout-main/layout-main.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from '../../blocks/header/header.component';
+import { FooterComponent } from '../../blocks/footer/footer.component';
 
 @Component({
   selector: 'app-layout-main',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeaderComponent, FooterComponent],
   templateUrl: './layout-main.component.html',
   styleUrl: './layout-main.component.css',
 })
