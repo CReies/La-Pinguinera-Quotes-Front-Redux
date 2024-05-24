85a54db41ebc3e990a514d5949dfad26190550ce
= <ccreyesmolina@gmail.com>
2024-05-24 12:49:29 -0500

    Update calculateGroupQuoteResult action to remove unnecessary props

--- src/app/core/store/actions/calculate-group-quote.actions.ts
+++ src/app/core/store/actions/calculate-group-quote.actions.ts
@@ -1,18 +1,12 @@
 import { createAction, props } from '@ngrx/store';
 import { ICalculateGroupQuoteResponse } from '../../models/api/response/calculate-quote/calculate-group-quote-response.model';
-import { ICart } from '../state-interfaces/group-cart.state';
 
 export const calculateGroupQuoteResult = createAction(
-  '[CalculateGroupQuote] Calculate Group Quote Result',
-  props<{
-    aggregateId: string;
-    cartGroups: ICart[];
-    customerRegisterDate: Date;
-  }>()
+  '[CalculateGroupQuote] Calculate Group Quote Result'
 );
 
 export const calculateGroupQuoteResultSuccess = createAction(
-  '[CalculateGroupQuote] Calculate Group Quote Result',
+  '[CalculateGroupQuote] Calculate Group Quote Result Success',
   props<ICalculateGroupQuoteResponse>()
 );
 
