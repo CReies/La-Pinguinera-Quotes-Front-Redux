69ed056ad3c92a9ac5a29d7206d87a17ab7d3c23
= <ccreyesmolina@gmail.com>
2024-05-24 02:37:31 -0500

    Update CustomerRegisterDate type to string in ICalculateGroupQuoteRequest interface

--- src/app/core/models/api/request/calculate-quote/calculate-group-quote-request.model.ts
+++ src/app/core/models/api/request/calculate-quote/calculate-group-quote-request.model.ts
@@ -3,5 +3,5 @@ import { IBookIdWithQuantity } from '../../../shared/book-id-with-quantity.model
 export interface ICalculateGroupQuoteRequest {
   AggregateId: string;
   Group: IBookIdWithQuantity[][];
-  CustomerRegisterDate: Date;
+  CustomerRegisterDate: string;
 }
