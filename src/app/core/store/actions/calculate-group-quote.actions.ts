8b4972e8607551dca80eb11f8c91479133aafc04
= <ccreyesmolina@gmail.com>
2024-05-24 02:38:05 -0500

    Refactor CalculateGroupQuoteService to use storage service for quote aggregate and register date

--- src/app/core/store/actions/calculate-group-quote.actions.ts
+++ src/app/core/store/actions/calculate-group-quote.actions.ts
@@ -1,16 +1,21 @@
 import { createAction, props } from '@ngrx/store';
 import { ICalculateGroupQuoteResponse } from '../../models/api/response/calculate-quote/calculate-group-quote-response.model';
-import { ICalculateGroupQuoteRequest } from '../../models/api/request/calculate-quote/calculate-group-quote-request.model';
+import { ICart } from '../state-interfaces/group-cart.state';
 
 export const calculateGroupQuoteResult = createAction(
   '[CalculateGroupQuote] Calculate Group Quote Result',
-  props<ICalculateGroupQuoteRequest>()
+  props<{
+    aggregateId: string;
+    cartGroups: ICart[];
+    customerRegisterDate: Date;
+  }>()
 );
 
 export const calculateGroupQuoteResultSuccess = createAction(
   '[CalculateGroupQuote] Calculate Group Quote Result',
   props<ICalculateGroupQuoteResponse>()
 );
+
 export const calculateGroupQuoteResultFailure = createAction(
   '[CalculateGroupQuote] Calculate Group Quote Result',
   props<{ error: string }>()
