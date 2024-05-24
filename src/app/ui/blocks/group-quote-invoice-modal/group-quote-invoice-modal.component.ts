18adec1d2d75b54600e03b242a086504772ae6fd
= <ccreyesmolina@gmail.com>
2024-05-24 07:30:41 -0500

    Update group quote invoice modal component and related files

--- src/app/ui/blocks/group-quote-invoice-modal/group-quote-invoice-modal.component.ts
+++ src/app/ui/blocks/group-quote-invoice-modal/group-quote-invoice-modal.component.ts
@@ -1,14 +1,25 @@
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { ICalculateGroupQuoteResponse } from '../../../core/models/api/response/calculate-quote/calculate-group-quote-response.model';
 import { CurrencyPipe } from '@angular/common';
+import { IModalState } from '../../../core/store/state-interfaces/modal.state';
+import { TitleComponent } from '../../elements/title/title.component';
+import { SvgComponent } from '../../elements/svg/svg.component';
 
 @Component({
   selector: 'app-group-quote-invoice-modal',
   standalone: true,
-  imports: [CurrencyPipe],
+  imports: [CurrencyPipe, TitleComponent, SvgComponent],
   templateUrl: './group-quote-invoice-modal.component.html',
   styleUrl: './group-quote-invoice-modal.component.css',
 })
 export class GroupQuoteInvoiceModalComponent {
   @Input() quoteResult: ICalculateGroupQuoteResponse;
+  @Input() modal: IModalState['groupQuoteModal'];
+
+  @Output() onClose: EventEmitter<void> = new EventEmitter();
+
+  close(event?: Event): void {
+    if (event && event.target !== event.currentTarget) return;
+    this.onClose.emit();
+  }
 }
