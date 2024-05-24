1f93dc842a96a29d3ceabe2879acfc343aa535e6
= <ccreyesmolina@gmail.com>
2024-05-24 02:38:25 -0500

    Refactor CalculateGroupQuoteService to use storage service for quote aggregate and register date

--- src/app/core/store/store.ts
+++ src/app/core/store/store.ts
@@ -4,20 +4,26 @@ import { BooksEffects } from './effects/books.effects';
 import { IBooksState } from './state-interfaces/books.state';
 import { IGroupCartState } from './state-interfaces/group-cart.state';
 import { groupCartReducer } from './reducers/group-cart.reducer';
+import { CalculateQuoteEffects } from './effects/calculate-quote.effects';
+import { ICalculateGroupQuoteState } from './state-interfaces/calculate-group-quote.state';
+import { calculateGroupQuoteReducer } from './reducers/calculate-group-quote.reducer';
 
 export interface AppState {
   books: IBooksState;
   groupCart: IGroupCartState;
+  calculateGroupQuote: ICalculateGroupQuoteState;
 }
 
 export interface AppStore {
   books: ActionReducer<IBooksState, Action>;
   groupCart: ActionReducer<IGroupCartState, Action>;
+  calculateGroupQuote: ActionReducer<ICalculateGroupQuoteState, Action>;
 }
 
 export const appStore: AppStore = {
   books: booksReducer,
   groupCart: groupCartReducer,
+  calculateGroupQuote: calculateGroupQuoteReducer,
 };
 
-export const appEffects = [BooksEffects];
+export const appEffects = [BooksEffects, CalculateQuoteEffects];
