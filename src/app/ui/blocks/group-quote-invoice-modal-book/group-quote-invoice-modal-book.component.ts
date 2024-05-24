da00dd8b52ba2d3644640da23f1158fe7360c44e
= <ccreyesmolina@gmail.com>
2024-05-24 12:48:50 -0500

    Update book details display in group-quote-invoice-modal-book component

--- src/app/ui/blocks/group-quote-invoice-modal-book/group-quote-invoice-modal-book.component.ts
+++ src/app/ui/blocks/group-quote-invoice-modal-book/group-quote-invoice-modal-book.component.ts
@@ -1,6 +1,8 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnChanges } from '@angular/core';
 import { IBookDetails } from '../../../core/models/shared/book-details.model';
 import { CurrencyPipe } from '@angular/common';
+import { bookTypeStringArray } from '../../../core/resources/book-type-dictionary';
+import { BookType } from '../../../core/models/enums/bookType';
 
 @Component({
   selector: 'app-group-quote-invoice-modal-book',
@@ -9,6 +11,12 @@ import { CurrencyPipe } from '@angular/common';
   templateUrl: './group-quote-invoice-modal-book.component.html',
   styleUrl: './group-quote-invoice-modal-book.component.css',
 })
-export class GroupQuoteInvoiceModalBookComponent {
+export class GroupQuoteInvoiceModalBookComponent implements OnChanges {
   @Input() book: IBookDetails;
+
+  bookType: string;
+
+  ngOnChanges(): void {
+    this.bookType = bookTypeStringArray[this.book.type];
+  }
 }
