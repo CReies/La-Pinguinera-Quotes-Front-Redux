243d95b48bd1b042a6a339f9d5febdd5fb03c716
= <ccreyesmolina@gmail.com>
2024-05-24 12:48:35 -0500

    Add loading-min component

---
+++ src/app/ui/elements/loading-min/loading-min.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-loading-min',
+  standalone: true,
+  imports: [],
+  templateUrl: './loading-min.component.html',
+  styleUrl: './loading-min.component.css'
+})
+export class LoadingMinComponent {
+
+}
