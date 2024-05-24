40bda5a8d44be990f30502fb8b3fae1df6c9c9d3
= <ccreyesmolina@gmail.com>
2024-05-24 02:37:53 -0500

    Refactor CalculateGroupQuoteService to use storage service for quote aggregate and register date

--- src/app/core/services/api/calculate-quote/calculate-group-quote.service.ts
+++ src/app/core/services/api/calculate-quote/calculate-group-quote.service.ts
@@ -5,19 +5,32 @@ import { CalculateGroupQuoteResponseMapper } from '../../../mappers/api/response
 import { ICalculateGroupQuoteResponse } from '../../../models/api/response/calculate-quote/calculate-group-quote-response.model';
 import { URL_RESOURCES } from '../../../resources/url.resources';
 import { Observable, map } from 'rxjs';
+import { StorageService } from '../../generals/storage.service';
+import { IQuoteAggregate } from '../../../models/shared/quote-aggregate.model';
 
 @Injectable({ providedIn: 'root' })
 export class CalculateGroupQuoteService {
   constructor(
     private readonly httpService: HttpService,
+    private readonly storageService: StorageService,
     private readonly reqMapper: CalculateGroupQuoteRequestMapper,
     private readonly resMapper: CalculateGroupQuoteResponseMapper
   ) {}
 
   exec(data): Observable<ICalculateGroupQuoteResponse> {
+    const { aggregateId } =
+      this.storageService.get<IQuoteAggregate>('quoteAggregate');
+    const customerRegisterDate = this.storageService.get<Date>('registerDate');
+
     const url = URL_RESOURCES.calculateQuote.group;
+    const mappedData = this.reqMapper.map({
+      aggregateId,
+      customerRegisterDate,
+      data,
+    });
+
     const result = this.httpService
-      .post<ICalculateGroupQuoteResponse>(url, this.reqMapper.map(data))
+      .post<ICalculateGroupQuoteResponse>(url, mappedData)
       .pipe(map((res) => this.resMapper.map(res)));
 
     return result;
