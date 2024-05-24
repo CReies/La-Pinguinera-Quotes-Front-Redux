084513dcef0d4a3c37b9e296c02498a63813bf7e
= <ccreyesmolina@gmail.com>
2024-05-24 07:31:01 -0500

    Update header component styling and structure

--- src/app/ui/blocks/header/header.component.ts
+++ src/app/ui/blocks/header/header.component.ts
@@ -1,11 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { SvgComponent } from '../../elements/svg/svg.component';
+import { RouterModule } from '@angular/router';
+import { AnchorComponent } from '../../elements/anchor/anchor.component';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [SvgComponent, AnchorComponent, RouterModule],
   templateUrl: './header.component.html',
-  styleUrl: './header.component.css'
+  styleUrl: './header.component.css',
 })
 export class HeaderComponent {
 
